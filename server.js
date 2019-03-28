2b060d1ac88d773a85cf7143ea7e83c6dda49a19
Aliya Summers <aliyasummers1@gmail.com>
2019-03-28 11:21:00 -0700

    API rendering after reroute

--- server.js
+++ server.js
@@ -24,17 +24,22 @@ client.on('error', err => console.log(err));
 app.set('view engine', 'ejs');
 
 //API routes
-//path to view page
-app.get('/', newSearch);
+//path to view page/home route
+//TODO: this route works fine no touch
+app.get('/searches/new', newBookSearchForm);
 
 //internal local host routes
+
 app.get('/books/:book_id', getOneBook)
 
 
+app.get('/books/show', getBooksFromDB)
 
+//TODO: bring back in when getBooks is fully resolved
+//app.post('/searches/show', addBookToDB)
 
 //creates a new search to Google Books API
-app.post('/searches', getBook);
+app.post('/searches/show', booksFromAPI);
 
 
 //catch-all
@@ -47,42 +52,14 @@ function handleError(err, res){
   if (res) res.status(500).send('You have achieved an error. Congratulations!');
 }
 
-//HELPER FUNCTIONS
 
-function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-  this.authors = info.authors || 'No author available';
-  this.title = info.title || 'No title available';
-  this.description = info.description || 'No description available';
-  this.isbn = info.isbn || 'No ISBN available';
-  this.thumbnail = info.imageLinks.thumbnail.replace('http://', 'https://') || placeholderImage;
+
+function newBookSearchForm (request, response) {
+  response.render('pages/searches/new');
 }
-
-
-function newSearch (request, response) {
-  response.render('pages/index');
-}
-
-//function to make query for book info
-function getBook(request, response) {
-  let url=`https://www.googleapis.com/books/v1/volumes?q=`;
-  console.log(request.body);
-  if (request.body.search[1] === 'title') {
-    url += `+intitle:${request.body.search[0]}`;
-  }
-  if (request.body.search[1] === 'author') {
-    url += `+inauthor:${request.body.search[0]}`;
-  }
-  superagent.get(url)
-    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render(`pages/searches/show`, {searchResults: results}))
-    .catch(error => handleError(error, response));
-}
-
-
 
 function getOneBook(request, response){
-  console.log('BOOK ID', request.params.task_id);
+  console.log('BOOK ID', request.params.book_id);
 
   let SQL = 'SELECT * FROM books WHERE id=$1;';
   let values = [request.params.book_id];
@@ -94,7 +71,17 @@ function getOneBook(request, response){
     .catch(error => handleError(error, response));
 }
 
-//TODO: we need to add a route for the function and modify the routes for the newSearch functions
+
+function getBooksFromDB (request, response){
+  let SQL = 'SELECT * FROM books;';
+
+  return client.query(SQL)
+    .then(bookResultsFromDB =>{
+      console.log(bookResultsFromDB);
+      response.render('/books/show', {bookResultsFromDB: bookResultsFromDB.rows})
+    })
+    .catch(handleError);
+}
 
 function addBookToDB(request, response){
   console.log(request.body);
@@ -114,6 +101,34 @@ function addBookToDB(request, response){
 
 
 
+function booksFromAPI(request, response) {
+  console.log('hit the books from API page')
+  let url=`https://www.googleapis.com/books/v1/volumes?q=`;
+  console.log(request.body);
+  if (request.body.search[1] === 'title') {
+    url += `+intitle:${request.body.search[0]}`;
+  }
+  if (request.body.search[1] === 'author') {
+    url += `+inauthor:${request.body.search[0]}`;
+  }
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render(`pages/searches/show`, {searchResults: results}))
+    .catch(error => handleError(error, response));
+}
+
+
+
+//HELPER FUNCTIONS
+
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.authors = info.authors || 'No author available';
+  this.title = info.title || 'No title available';
+  this.description = info.description || 'No description available';
+  this.isbn = info.isbn || 'No ISBN available';
+  this.thumbnail = info.imageLinks.thumbnail.replace('http://', 'https://') || placeholderImage;
+}
 
 
 
@@ -124,61 +139,62 @@ function addBookToDB(request, response){
 
 
 
-/*====================================CORPSE CODE==================*/
-//we want to save this code where we can easily access it again when we start refactoring for sql to the specs of the lab.
-//we got ahead of ourselves, and this function stopped rendering pages.
+
+// ======================CORPRSE CODE=================
+//brings you to the original search form
+// function newBookSearch (request, response) {
+//   response.render('pages/index');
+// }
 
 //function to make query for book info
-// function getBook(request, response) {
-//   console.log('hello from get books');
-//   const selectSQL = `SELECT * FROM books where search_query=$1;`;
-//   //TODO: also broken
-//   //const values = [request.query.info];
-//   const values = [request.params.content];
-
-
-//   client.query(selectSQL)
-//     .then(result => {
-//       if(result.rowCount > 0){
-//         response.send(result.rows[0]);
-//       } else{
-//         // beginning of url
-//         console.log('===========this is request.body.search', request.body.search)
-//         let url=`https://www.googleapis.com/books/v1/volumes?q=`;
-//         if (request.body.search[1] === 'title') {
-//           url += `+intitle:${request.body.search[0]}`;
-//         }
-//         if (request.body.search[1] === 'author') {
-//           url += `+inauthor:${request.body.search[0]}`;
-//         }
-//         //end of url
-//         superagent.get(url)
-//           .then(apiResponse => {
-//             console.log('========API RESPONSE==========', apiResponse.body)
-
-//             if (!apiResponse.body.items.volumeInfo.title) {
-//               throw 'NO BOOK INFORMATION';
-//             } else {
-//               let loggedBook = new Book(bookResult.volumeInfo, request.query);
-
-//               let insertSql = `INSERT INTO books (authors, title, description, isbn, thumbnail) VALUES($1, $2, $3, $4, $5) RETURNING id;`;
-//               let newValues = Object.values(loggedBook);
-
-//               client.query(insertSql, newValues)
-//                 .then(sqlReturn => {
-//                   loggedBook.id = sqlReturn.rows[0].id;
-//                   response.send(loggedBook);
-//                 });
-//             }
-//           });
-//       }
-//     })
-
-//     .catch(error => handleError(error, response))
-//     // .then(apiResponse => apiResponse.body.items.map(bookResult =>{
-//     //   return new Book(bookResult.volumeInfo);
-//     // }))
-//     // .then(results => response.render(`pages/searches/show`, {searchResults: results}))
-//     // .catch(error => handleError(error, response))
+// function getBooks(request, response) {
+//   let url=`https://www.googleapis.com/books/v1/volumes?q=`;
+//   console.log(request.body);
+//   if (request.body.search[1] === 'title') {
+//     url += `+intitle:${request.body.search[0]}`;
+//   }
+//   if (request.body.search[1] === 'author') {
+//     url += `+inauthor:${request.body.search[0]}`;
+//   }
+//   superagent.get(url)
+//     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+//     .then(results => response.render(`pages/searches/show`, {searchResults: results}))
+//     .catch(error => handleError(error, response));
 // }
+
+
+
+// function getOneBook(request, response){
+//   console.log('BOOK ID', request.params.book_id);
+
+//   let SQL = 'SELECT * FROM books WHERE id=$1;';
+//   let values = [request.params.book_id];
+
+//   return client.query(SQL, values)
+//     .then(bookResult =>{
+//       return response.render('pages/detail', {book: bookResult.rows[0]});
+//     })
+//     .catch(error => handleError(error, response));
+// }
+
+//TODO: add getBooksFromDB function
+
+//TODO: we need to add a route for the function and modify the routes for the newSearch functions
+
+// function addBookToDB(request, response){
+//   console.log(request.body);
+
+//   let{authors, title, description, isbn, thumbnail} = request.body;
+
+//   let SQL= 'INSERT INTO books(authors, title, description, isbn, thumbnail) VALUES ($1, $2, $3, $4, $5);';
+//   let values = [authors, title, description, isbn, thumbnail];
+
+//   return client.query(SQL, values)
+//     .then(sqlResult =>{
+//       console.log(sqlResult);
+//       response.redirect('/')
+//     })
+//     .catch(error => handleError(error, response));
+// }
+
 
